b877f7ac0e00362525a5b827a914020bc0c3b1f5
TimPhillip <timphillip.schneider@t-online.de>
2021-03-26 12:58:06 +0100

    resolved merge conflicts

--- org.planqk.nisq.analyzer.core/src/test/java/org/planqk/nisq/analyzer/core/ImplementationSelectionTest.java
+++ org.planqk.nisq.analyzer.core/src/test/java/org/planqk/nisq/analyzer/core/ImplementationSelectionTest.java
@@ -19,43 +19,19 @@
 
 package org.planqk.nisq.analyzer.core;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 
-import org.apache.tomcat.jni.Time;
-import org.hibernate.service.spi.InjectService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.Spy;
-import org.planqk.nisq.analyzer.core.Application;
-import org.planqk.nisq.analyzer.core.connector.SdkConnector;
-import org.planqk.nisq.analyzer.core.connector.qiskit.QiskitSdkConnector;
 import org.planqk.nisq.analyzer.core.control.NisqAnalyzerControlService;
 import org.planqk.nisq.analyzer.core.model.AnalysisJob;
-import org.planqk.nisq.analyzer.core.model.Implementation;
 import org.planqk.nisq.analyzer.core.model.Provider;
-import org.planqk.nisq.analyzer.core.model.Qpu;
-import org.planqk.nisq.analyzer.core.model.Sdk;
 import org.planqk.nisq.analyzer.core.qprov.QProvService;
-import org.planqk.nisq.analyzer.core.repository.ImplementationRepository;
-import org.planqk.nisq.analyzer.core.repository.ImplementationSelectionJobRepository;
-import org.planqk.nisq.analyzer.core.repository.SdkRepository;
+import org.planqk.nisq.analyzer.core.repository.AnalysisJobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 
 
 public class ImplementationSelectionTest extends NisqAnalyzerTestCase {
@@ -64,7 +40,7 @@ public class ImplementationSelectionTest extends NisqAnalyzerTestCase {
     private QProvService qProvService;
 
     @Autowired
-    protected ImplementationSelectionJobRepository implementationSelectionJobRepository;
+    protected AnalysisJobRepository analysisJobRepository;
 
     @Autowired
     protected NisqAnalyzerControlService nisqAnalyzerControlService;
@@ -90,7 +66,7 @@ public class ImplementationSelectionTest extends NisqAnalyzerTestCase {
     @Test
     public void testSelectionShor15(){
 
-        AnalysisJob job = implementationSelectionJobRepository.save(new AnalysisJob());
+        AnalysisJob job = analysisJobRepository.save(new AnalysisJob());
         nisqAnalyzerControlService.performSelection(job,shorAlgorithmUuid, composeShorInputParameters(15));
 
         assertContainsImpl(job, "shor15-qiskit");
@@ -111,7 +87,7 @@ public class ImplementationSelectionTest extends NisqAnalyzerTestCase {
     @Test
     public void testSelectionShor9(){
 
-        AnalysisJob job = implementationSelectionJobRepository.save(new AnalysisJob());
+        AnalysisJob job = analysisJobRepository.save(new AnalysisJob());
         nisqAnalyzerControlService.performSelection(job,shorAlgorithmUuid, composeShorInputParameters(9));
 
         assertNotContainsImpl(job, "shor15-qiskit");
@@ -128,7 +104,7 @@ public class ImplementationSelectionTest extends NisqAnalyzerTestCase {
     @Test
     public void testSelectionShor6(){
 
-        AnalysisJob job = implementationSelectionJobRepository.save(new AnalysisJob());
+        AnalysisJob job = analysisJobRepository.save(new AnalysisJob());
         nisqAnalyzerControlService.performSelection(job,shorAlgorithmUuid, composeShorInputParameters(6));
 
         assertNotContainsImpl(job, "shor15-qiskit");
@@ -139,7 +115,7 @@ public class ImplementationSelectionTest extends NisqAnalyzerTestCase {
 
     @Test
     public void testSelectionGroverFix(){
-        AnalysisJob job = implementationSelectionJobRepository.save(new AnalysisJob());
+        AnalysisJob job = analysisJobRepository.save(new AnalysisJob());
         nisqAnalyzerControlService.performSelection(job,groverTruthtableUuid, composeGroverInputParameters("0010000000000000"));
 
         assertContainsImpl(job, "grover-fix-truthtable-qiskit");
@@ -151,7 +127,7 @@ public class ImplementationSelectionTest extends NisqAnalyzerTestCase {
 
     @Test
     public void testSelectionGroverGeneral(){
-        AnalysisJob job = implementationSelectionJobRepository.save(new AnalysisJob());
+        AnalysisJob job = analysisJobRepository.save(new AnalysisJob());
         nisqAnalyzerControlService.performSelection(job,groverTruthtableUuid, composeGroverInputParameters("0001000000000000"));
 
         assertNotContainsImpl(job, "grover-fix-truthtable-qiskit");
@@ -163,7 +139,7 @@ public class ImplementationSelectionTest extends NisqAnalyzerTestCase {
 
     @Test
     public void testSelectionGroverGeneralSmall(){
-        AnalysisJob job = implementationSelectionJobRepository.save(new AnalysisJob());
+        AnalysisJob job = analysisJobRepository.save(new AnalysisJob());
         nisqAnalyzerControlService.performSelection(job,groverTruthtableUuid, composeGroverInputParameters("0001"));
 
         assertNotContainsImpl(job, "grover-fix-truthtable-qiskit");
@@ -175,7 +151,7 @@ public class ImplementationSelectionTest extends NisqAnalyzerTestCase {
 
     @Test
     public void testSelectionGroverSAT(){
-        AnalysisJob job = implementationSelectionJobRepository.save(new AnalysisJob());
+        AnalysisJob job = analysisJobRepository.save(new AnalysisJob());
         nisqAnalyzerControlService.performSelection(job,groverSatUuid, composeGroverSATInputParameters("(A | B) & (A | ~B) & (~A | B)"));
 
         assertContainsImpl(job, "grover-fix-sat-qiskit");
@@ -189,7 +165,7 @@ public class ImplementationSelectionTest extends NisqAnalyzerTestCase {
 
     @Test
     public void testSelectionGroverSATGeneral(){
-        AnalysisJob job = implementationSelectionJobRepository.save(new AnalysisJob());
+        AnalysisJob job = analysisJobRepository.save(new AnalysisJob());
         nisqAnalyzerControlService.performSelection(job,groverSatUuid, composeGroverSATInputParameters("(A | B)"));
 
         assertNotContainsImpl(job, "grover-fix-sat-qiskit");
