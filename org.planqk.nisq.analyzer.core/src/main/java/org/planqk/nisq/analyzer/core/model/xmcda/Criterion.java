b16e2e51eea464ab9dce3bd707397e8924f60a13
wederbn <benjamin.weder@iaas.uni-stuttgart.de>
2021-08-06 17:50:05 +0200

    Add wrapper for XMCDA criterion for the repository

---
+++ org.planqk.nisq.analyzer.core/src/main/java/org/planqk/nisq/analyzer/core/model/xmcda/Criterion.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2021 University of Stuttgart
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+package org.planqk.nisq.analyzer.core.model.xmcda;
+
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+public class Criterion extends org.xmcda.v2.Criterion {
+
+    @Id
+    @Getter
+    @Setter
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID uuid;
+}
