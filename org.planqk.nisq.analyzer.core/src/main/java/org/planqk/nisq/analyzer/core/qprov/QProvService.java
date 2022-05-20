1815fd3d6e6ff8d4bdb6379746cd8920dc2b814a
Maximilian Kuhn <maximilian.kuhn@ymail.com>
2022-05-20 16:28:36 +0200

    Removed QProv Avoidance
    Since we were able to suppy QProv with data regarding Cirq QPUs, it no longer needed to be handled in the code.

--- org.planqk.nisq.analyzer.core/src/main/java/org/planqk/nisq/analyzer/core/qprov/QProvService.java
+++ org.planqk.nisq.analyzer.core/src/main/java/org/planqk/nisq/analyzer/core/qprov/QProvService.java
@@ -57,18 +57,14 @@ public class QProvService {
         RestTemplate restTemplate = new RestTemplate();
         try {
             ProviderListDto result = restTemplate.getForObject(this.baseAPIEndpoint + "providers", ProviderListDto.class);
-
             List<Provider> providerList;
             if (result != null && result.getEmbedded() != null) {
-                //Since QProv does not currently support the Cirq Google Provider, it needs to be manually added to the list
+                // In case the result isn't empty, extract it.
                 providerList = ProviderListDto.Converter.convert(result);
             } else {
-                //In case QProv does not return providers, just return the locally supported cirq-google provider
+                //In case QProv does not return providers, return an empty list.
                 providerList = new ArrayList<>();
             }
-            Provider googleProv = new Provider();
-            googleProv.setName(Constants.GOOGLE);
-            providerList.add(googleProv);
             return providerList;
         } catch (RestClientException e) {
             return new ArrayList<>();
@@ -76,45 +72,12 @@ public class QProvService {
     }
 
     public List<Qpu> getQPUs(Provider provider) {
-        // Since QProv does not currently support Google QPUs, they are added Manually with filler informations
-        if (provider.getName().equalsIgnoreCase(Constants.GOOGLE)) {
-            // All supported Google QPUs
-            List<String> names = new ArrayList<>(4);
-            names.add("sycamore");
-            names.add("sycamore23");
-            names.add("bristlecone");
-            names.add("foxtail");
-            List<Qpu> qpus = new ArrayList<>(4);
-            for (String name: names) {
-                // Adding real information
-                Qpu qpu = new Qpu();
-                qpu.setProvider(Constants.GOOGLE);
-                // Currently, while code can be transpiled for these real QPUs, the execution can only be simulated due to missing access tokens.
-                qpu.setSimulator(true);
-                qpu.setName(name);
-                // Add dummy information
-                qpu.setAvgMultiQubitGateError(-1);
-                qpu.setAvgMultiQubitGateTime(-1);
-                qpu.setAvgReadoutError(-1);
-                qpu.setMaxGateTime(-1);
-                qpu.setAvgReadoutError(-1);
-                qpu.setAvgSingleQubitGateError(-1);
-                qpu.setAvgSingleQubitGateTime(-1);
-                qpu.setQubitCount(-1);
-                qpu.setQueueSize(-1);
-                qpu.setT1(-1);
-                qpu.setT2(-1);
-                qpus.add(qpu);
-            }
-            return qpus;
-        } else {
-            RestTemplate restTemplate = new RestTemplate();
-            // ToDo: Implement proper QPU List class
-            QpuListDto qpuListDto =
-                    restTemplate.getForObject(String.format(this.baseAPIEndpoint + "/providers/%s/qpus", provider.getId()), QpuListDto.class);
+        RestTemplate restTemplate = new RestTemplate();
+        // ToDo: Implement proper QPU List class
+        QpuListDto qpuListDto =
+                restTemplate.getForObject(String.format(this.baseAPIEndpoint + "/providers/%s/qpus", provider.getId()), QpuListDto.class);
 
-            return qpuListDto.getQpuDtoList().stream().map(dto -> QpuDto.Converter.convert(dto, provider.getName())).collect(Collectors.toList());
-        }
+        return qpuListDto.getQpuDtoList().stream().map(dto -> QpuDto.Converter.convert(dto, provider.getName())).collect(Collectors.toList());
     }
 
     public Optional<Qpu> getQpuByName(String name, String provider) {
