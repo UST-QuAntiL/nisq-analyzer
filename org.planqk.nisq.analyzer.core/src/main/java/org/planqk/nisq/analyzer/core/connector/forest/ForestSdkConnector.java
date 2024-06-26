6f7a2913570d8fe4414db0147fffdc2aef5bca69
Marie Salm <marie.salm@iaas.uni-stuttgart.de>
2024-06-26 18:12:58 +0200

    adapt forest connector for circuit generation and post processing

--- org.planqk.nisq.analyzer.core/src/main/java/org/planqk/nisq/analyzer/core/connector/forest/ForestSdkConnector.java
+++ org.planqk.nisq.analyzer.core/src/main/java/org/planqk/nisq/analyzer/core/connector/forest/ForestSdkConnector.java
@@ -19,6 +19,7 @@
 
 package org.planqk.nisq.analyzer.core.connector.forest;
 
+import static org.planqk.nisq.analyzer.core.connector.ConnectorUtils.calculateHistogramIntersection;
 import static org.planqk.nisq.analyzer.core.web.Utils.getBearerTokenFromRefreshToken;
 
 import java.io.File;
@@ -32,6 +33,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 import org.apache.commons.io.FileUtils;
@@ -46,11 +48,13 @@ import org.planqk.nisq.analyzer.core.model.ExecutionResultStatus;
 import org.planqk.nisq.analyzer.core.model.Implementation;
 import org.planqk.nisq.analyzer.core.model.Parameter;
 import org.planqk.nisq.analyzer.core.model.ParameterValue;
+import org.planqk.nisq.analyzer.core.model.QpuSelectionResult;
 import org.planqk.nisq.analyzer.core.repository.ExecutionResultRepository;
 import org.planqk.nisq.analyzer.core.repository.QpuSelectionResultRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
@@ -68,6 +72,8 @@ public class ForestSdkConnector implements SdkConnector {
     private int pollInterval;
 
     // API Endpoints
+    private URI generateAPIEndpoint;
+
     private URI analyzeOriginalAPIEndpoint;
 
     private URI transpileAPIEndpoint;
@@ -78,12 +84,79 @@ public class ForestSdkConnector implements SdkConnector {
                               @Value("${org.planqk.nisq.analyzer.connector.forest.port}") int port,
                               @Value("${org.planqk.nisq.analyzer.connector.forest.version}") String version) {
         // compile the API endpoints
+        generateAPIEndpoint =
+            URI.create(String.format("http://%s:%d/forest-service/api/%s/generate-circuit", hostname, port, version));
         analyzeOriginalAPIEndpoint = URI.create(
             String.format("http://%s:%d/forest-service/api/%s/analyze-original-circuit", hostname, port, version));
         transpileAPIEndpoint =
             URI.create(String.format("http://%s:%d/forest-service/api/%s/transpile", hostname, port, version));
         executeAPIEndpoint =
             URI.create(String.format("http://%s:%d/forest-service/api/%s/execute", hostname, port, version));
+    }
+
+    public CircuitInformationOfImplementation getCircuitOfImplementation(Implementation implementation,
+                                                                         Map<String, ParameterValue> parameters,
+                                                                         String refreshToken) {
+        LOG.debug(
+            "Generating and analyzing quantum circuit of quantum algorithm implementation with forest Sdk connector " +
+                "plugin!");
+        String bearerToken = getBearerTokenFromRefreshToken(refreshToken)[0];
+        ForestRequest request =
+            new ForestRequest(implementation.getFileLocation(), implementation.getLanguage(), parameters, bearerToken);
+
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            // request to generate circuit
+            URI circuitLocation = restTemplate.postForLocation(generateAPIEndpoint, request);
+
+            ExecutionResultStatus generationComplete = ExecutionResultStatus.RUNNING;
+
+            // poll the forest service frequently
+            while (generationComplete != ExecutionResultStatus.FAILED) {
+                try {
+                    ResponseEntity<CircuitInformationOfImplementation> response =
+                        restTemplate.exchange(circuitLocation, HttpMethod.GET, null,
+                            CircuitInformationOfImplementation.class);
+
+                    CircuitInformationOfImplementation result = null;
+
+                    // Check if the forest service was successful
+                    if (response.getStatusCode().is2xxSuccessful()) {
+                        LOG.debug("Generating circuit using forest Service.");
+                        result = response.getBody();
+                        result.setCircuitLanguage(Constants.QUIL);
+
+                        // Check if generation is completed
+                        if (result.isComplete()) {
+                            generationComplete = ExecutionResultStatus.FINISHED;
+                            return result;
+                        }
+                    } else if (response.getStatusCode().is4xxClientError()) {
+                        LOG.error(
+                            String.format("Forest Service rejected request (HTTP %d)", response.getStatusCodeValue()));
+                        generationComplete = ExecutionResultStatus.FAILED;
+                    } else if (response.getStatusCode().is5xxServerError()) {
+                        LOG.error(
+                            String.format("Internal forest Service error (HTTP %d)", response.getStatusCodeValue()));
+                        generationComplete = ExecutionResultStatus.FAILED;
+                    }
+
+                    // Wait for next poll
+                    try {
+                        Thread.sleep(pollInterval);
+                    } catch (InterruptedException e) {
+                        // pass
+                    }
+                } catch (RestClientException e) {
+                    LOG.error("Polling generation result from forest Service failed.");
+                    generationComplete = ExecutionResultStatus.FAILED;
+                }
+            }
+        } catch (RestClientException e) {
+            LOG.error("Circuit generation with forest Service failed.");
+            throw new RuntimeException(e);
+        }
+        return null;
     }
 
     @Override
@@ -94,12 +167,13 @@ public class ForestSdkConnector implements SdkConnector {
                                                 QpuSelectionResultRepository qpuSelectionResultRepository,
                                                 String correlationId, URL fileLocation) {
         LOG.debug("Executing circuit passed as file with provider '{}' and qpu '{}'.", providerName, qpuName);
-        ForestRequest request = new ForestRequest(transpiledCircuit, qpuName, parameters);
-        executeQuantumCircuit(request, executionResult, resultRepository);
+        ForestRequest request = new ForestRequest(transpiledCircuit, qpuName, parameters, correlationId, fileLocation);
+        executeQuantumCircuit(request, executionResult, resultRepository, qpuSelectionResultRepository);
     }
 
     private void executeQuantumCircuit(ForestRequest request, ExecutionResult executionResult,
-                                       ExecutionResultRepository resultRepository) {
+                                       ExecutionResultRepository resultRepository,
+                                       QpuSelectionResultRepository qpuSelectionResultRepository) {
         RestTemplate restTemplate = new RestTemplate();
         try {
             // make the execution request
@@ -123,6 +197,28 @@ public class ForestSdkConnector implements SdkConnector {
                         executionResult.setStatusCode("Execution successfully completed.");
                         executionResult.setResult(result.getResult().toString());
                         executionResult.setShots(result.getShots());
+                        resultRepository.save(executionResult);
+                        executionResult.setResultLocation(resultLocation);
+
+                        // histogram intersection
+                        //FIXME currently only for qpu-selection
+                        if (Objects.nonNull(qpuSelectionResultRepository)) {
+                            Optional<QpuSelectionResult> qpuSelectionResult =
+                                qpuSelectionResultRepository.findById(executionResult.getQpuSelectionResult().getId());
+                            if (qpuSelectionResult.isPresent()) {
+                                // get stored token for the execution
+                                QpuSelectionResult qResult = qpuSelectionResult.get();
+
+                                // check if target machine is of Rigetti or IBMQ, consider accordingly qvm simulator
+                                // or ibmq simulator
+                                String simulator = "qvm";
+
+                                // check if current execution result is already of a simulator otherwise get all
+                                // qpu-selection-results of same job
+                                calculateHistogramIntersection(executionResult, resultRepository,
+                                    qpuSelectionResultRepository, result, qResult, simulator);
+                            }
+                        }
                         resultRepository.save(executionResult);
                     }
 
@@ -275,13 +371,6 @@ public class ForestSdkConnector implements SdkConnector {
         } catch (IOException e) {
             LOG.error("Unable to read file content from circuit file!");
         }
-        return null;
-    }
-
-    @Override
-    public CircuitInformationOfImplementation getCircuitOfImplementation(Implementation implementation,
-                                                                         Map<String, ParameterValue> parameters,
-                                                                         String refreshToken) {
         return null;
     }
 }
